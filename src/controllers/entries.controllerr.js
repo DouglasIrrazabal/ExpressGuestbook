939986e3d2bb23fb0026b3e5ac2fa1f5259da8e5
Douglas <douglasirrazabal@gmail.com>
2020-03-18 01:03:29 -0300

    finished

--- src/controllers/entries.controllerr.js
+++ src/controllers/entries.controllerr.js
@@ -1,14 +1,21 @@
+const entries = [];
 
 const renderIndex = (req, res) => {
-    res.render('index');
+    res.render('index', {entries});
 };
 
 const renderNewEntry = (req, res) => {
-    
+    res.render('new-entry');
 };
 
 const createNewEntry = (req, res) => {
-    
+    const newEntry = {
+        title: req.body.title,
+        content: req.body.body,
+        published: new Date()
+    };
+    entries.push(newEntry);
+    res.redirect('/');
 };
 
 module.exports = {
--- src/routes/entries.routes.js
+++ src/routes/entries.routes.js
@@ -4,7 +4,9 @@ const router = Router();
 const { renderIndex, renderNewEntry, createNewEntry } = require('../controllers/entries.controllerr');
 
 router.get('/', renderIndex);
+
 router.get('/new-entry', renderNewEntry);
+
 router.post('/new-entry', createNewEntry);
 
 module.exports = router;
